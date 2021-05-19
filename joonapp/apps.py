751e52c11c9e8ff5c2ceb01d2d34682fd67992ba
Alicia-krynne <alicemacrineadhiambo@gmail.com>
2021-05-20 02:49:23 +0300

    set initial files

---
+++ joonapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class JoonappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'joonapp'
---
+++ joonflix/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for joonflix project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'joonflix.settings')
+
+application = get_asgi_application()
